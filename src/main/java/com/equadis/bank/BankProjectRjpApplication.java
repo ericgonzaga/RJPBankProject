6ea2d15cc401d607caf8486ee3f81b24955de5b6
Eric Gonzaga <ericgonzaga@pbh.gov.br>
2023-11-12 16:49:27 +0000

    Porject init

---
+++ src/main/java/com/equadis/bank/BankProjectRjpApplication.java
@@ -0,0 +1,13 @@
+package com.equadis.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankProjectRjpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankProjectRjpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equadis/bank/BankProjectRjpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equadis.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProjectRjpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
